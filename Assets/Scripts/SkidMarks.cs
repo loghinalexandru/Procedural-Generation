4124d2f94da9d84976805b178dd5c8e2557a8c98
loghinalexandru <loghinalexandru61@gmail.com>
2019-02-24 19:06:14 +0200

    VisualUpdate

---
+++ Assets/Scripts/SkidMarks.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkidMarks : MonoBehaviour
+{
+    public GameObject player;
+    public TrailRenderer skidMarkR;
+    public TrailRenderer skidMarkL;
+    public float minSkew = 30.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (player.transform.eulerAngles.y % 90 >= minSkew && player.transform.eulerAngles.y % 90 <= 90 - minSkew)
+        {
+            skidMarkL.emitting = true;
+            skidMarkR.emitting = true;
+        }
+        else
+        {
+            skidMarkL.emitting = false;
+            skidMarkR.emitting = false;
+        }
+    }
+}
