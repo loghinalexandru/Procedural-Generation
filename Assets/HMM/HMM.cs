9e046bc408c5a392b7e0c1e5946488a2c3fac3d4
loghinalexandru <loghinalexandru61@gmail.com>
2019-03-08 10:54:51 +0200

    Changes

---
+++ Assets/HMM/HMM.cs
@@ -0,0 +1,126 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HMM : MonoBehaviour
+{
+    private List<List<float>> transitionProbabilities;
+    private List<List<float>> emissionProbabilities;
+    private int currentStateIndex = 0;
+    public List<float> startProbabilities;
+    public List<GameObject> emissions;
+    public TextAsset transitionFile;
+    public TextAsset emissionFile;
+
+    private void Init()
+    {
+        this.emissionProbabilities = new List<List<float>>();
+        this.transitionProbabilities = new List<List<float>>();
+        this.SetTransitionProbabilities();
+        this.SetEmissionProbabilities();
+        this.SetInitialState();
+    }
+
+    private void SetTransitionProbabilities()
+    {
+        string[] text = this.transitionFile.text.Split('\n');
+        for (int i = 0; i < text.Length; ++i)
+        {
+            List<float> line = new List<float>();
+            string[] probabilities = text[i].Split(' ');
+            for (int j = 0; j < probabilities.Length; ++j)
+            {
+                line.Add(float.Parse(probabilities[j]));
+            }
+            this.transitionProbabilities.Add(line);
+        }
+    }
+
+    private void SetEmissionProbabilities()
+    {
+        string[] text = this.emissionFile.text.Split('\n');
+        for (int i = 0; i < text.Length; ++i)
+        {
+            List<float> line = new List<float>();
+            string[] probabilities = text[i].Split(' ');
+            for (int j = 0; j < probabilities.Length; ++j)
+            {
+                line.Add(float.Parse(probabilities[j]));
+            }
+            this.emissionProbabilities.Add(line);
+        }
+    }
+
+
+    private void SetInitialState()
+    {
+        double start = 0.0f;
+        double end = 0.0f;
+        float randomValue = Random.value;
+        for (int i = 0; i < this.startProbabilities.Count; ++i)
+        {
+            start = end;
+            end += startProbabilities[i];
+            if (start <= randomValue && randomValue <= end)
+            {
+                this.currentStateIndex = i;
+                break;
+            }
+        }
+    }
+
+    private int MakeTransition(int state)
+    {
+        int index = -1;
+        double start = 0.0f;
+        double end = 0.0f;
+        float randomValue = Random.value;
+        for (int j = 0; j < this.transitionProbabilities[state].Count; ++j)
+        {
+            start = end;
+            end += this.transitionProbabilities[state][j];
+            if (start < randomValue && randomValue < end)
+            {
+                index = j;
+                break;
+            }
+        }
+        return index;
+    }
+
+    private int MakeEmission(int state)
+    {
+        int index = -1;
+        double start = 0.0f;
+        double end = 0.0f;
+        float randomValue = Random.value;
+        for (int j = 0; j < this.emissionProbabilities[state].Count; ++j)
+        {
+            start = end;
+            end += this.emissionProbabilities[state][j];
+            if (start < randomValue && randomValue < end)
+            {
+                index = j;
+                break;
+            }
+        }
+        return index;
+    }
+
+    public GameObject NextEmission()
+    {
+        int emissionIndex = MakeEmission(this.currentStateIndex);
+        while (emissionIndex == -1)
+        {
+            this.currentStateIndex = MakeTransition(currentStateIndex);
+            emissionIndex = MakeEmission(this.currentStateIndex);
+        }
+        this.currentStateIndex = MakeTransition(currentStateIndex);
+        return this.emissions[emissionIndex];
+    }
+
+    void Start()
+    {
+        Init();
+    }
+}
--- Assets/Scripts/GeneratePlatform.cs
+++ Assets/Scripts/GeneratePlatform.cs
@@ -6,8 +6,7 @@ using UnityEngine.EventSystems;
 
 public class GeneratePlatform : MonoBehaviour
 {
-    private Markov generator;
-    private HHMM platformGenerator;
+    private HMM generator;
     public GameObject currentPlatform;
     public float platformSize = 20;
     public int propsPerPlatform = 2;
@@ -22,9 +21,8 @@ public class GeneratePlatform : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        this.generator = this.GetComponent<Markov>();
-        this.platformGenerator = GameObject.FindGameObjectWithTag("Generator").GetComponent<HHMM>();
-        this.zOffset = generator.states[0].transform.localScale.z;
+        this.generator = GameObject.FindGameObjectWithTag("Generator").GetComponent<HMM>();
+        this.zOffset = generator.emissions[0].transform.localScale.z;
         this.platformSize = this.zOffset;
     }
 
@@ -91,37 +89,34 @@ public class GeneratePlatform : MonoBehaviour
     public GameObject BuildPlatform(GameObject state)
     {
         float nextObjectDistance = 0;
-        List<GameObject> props = generator.GenerateEmissions(state, this.propsPerPlatform);
+        //List<GameObject> props = generator.GenerateEmissions(state, this.propsPerPlatform);
         GameObject nextPlatform = Instantiate(state, new Vector3(this.currentPlatform.transform.position.x + this.xOffset, -15, this.currentPlatform.transform.position.z + this.zOffset), Quaternion.Euler(0, 0, 0));
-        for (int i = 0; i < props.Count; ++i)
-        {
-            float side = this.SideGeneration(nextPlatform.name);
-            int propRotation = 0;
-            if (side < 0)
-                propRotation = 180;
-            props[i] = Instantiate(props[i], new Vector3(nextPlatform.transform.position.x + side, nextPlatform.transform.position.y, nextPlatform.transform.position.z - this.platformSize / 2 + 2 * nextObjectDistance), Quaternion.Euler(0, propRotation, 0));
-            nextObjectDistance += props[i].transform.localScale.z + this.distanceBetweenProps;
-            props[i].transform.SetParent(nextPlatform.transform);
-        }
+        //for (int i = 0; i < props.Count; ++i)
+        //{
+        //    float side = this.SideGeneration(nextPlatform.name);
+        //    int propRotation = 0;
+        //    if (side < 0)
+        //        propRotation = 180;
+        //    props[i] = Instantiate(props[i], new Vector3(nextPlatform.transform.position.x + side, nextPlatform.transform.position.y, nextPlatform.transform.position.z - this.platformSize / 2 + 2 * nextObjectDistance), Quaternion.Euler(0, propRotation, 0));
+        //    nextObjectDistance += props[i].transform.localScale.z + this.distanceBetweenProps;
+        //    props[i].transform.SetParent(nextPlatform.transform);
+        //}
         nextPlatform.transform.rotation = Quaternion.Euler(0, this.rotation, 0);
         return nextPlatform;
     }
 
     public void InstantiatePlatform()
     {
-        GameObject nextPlatform = BuildPlatform(platformGenerator.NextEmission());
-        switch (nextPlatform.name)
+        GameObject nextPlatform = BuildPlatform(generator.NextEmission());
+        if (nextPlatform.name.Contains("Left"))
         {
-            case "Right(Clone)":
-                this.rotation += 90;
-                this.SetOffset();
-                break;
-            case "Left(Clone)":
-                this.rotation -= 90;
-                this.SetOffset();
-                break;
-            default:
-                break;
+            this.rotation -= 90;
+            this.SetOffset();
+        }
+        if (nextPlatform.name.Contains("Right"))
+        {
+            this.rotation += 90;
+            this.SetOffset();
         }
         DestroyCurrentPlatform();
         this.currentPlatform = nextPlatform;
